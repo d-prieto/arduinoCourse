27a69d690c986d2a6ef4c16f6b710f5abc64887d
d-prieto <60569015+d-prieto@users.noreply.github.com>
2021-02-11 15:25:59 +0100

    Update plantilla.ino

--- plantilla.ino
+++ plantilla.ino
@@ -1,1 +1,25 @@
+/*
+*  Documentación del programa 
+*  Autore: 
+*  Fecha:
+*  Código referencia: (url)
+*  Descripción del programa:
+*  Hardware necesario:
+*/
 
+//Includes
+
+//Definiciones de constantes de pines
+
+//Definición de variables globales 
+
+void setup() {
+//inicializaciones
+//código que se ejecuta solo una vez. Calibraciones  
+}
+
+void loop() {
+ //código que se repite una y otra vez. Recuerda colocar las funciones que hagan falta para que sea más claro.  
+}  
+
+// Definición de funciones necesarias para el funcionamiento del loop y el setup "calibrate()" "checkButton()" "sound()" o lo que haga el programa en cuestión. 
